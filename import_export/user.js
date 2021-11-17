68f4522134c38ae5f1dfa0be96f9d2678adba2fd
Tống Đức Nam <637849@sv.vnua.edu.vn>
2021-11-17 21:49:21 +0700

    update1_17_11

--- import_export/user.js
+++ import_export/user.js
@@ -1,6 +1,26 @@
 // ==================import and export ==============
-export let User_name = "Javascript";
-const numbers = [1,2,3,4];
+// export let User_name = "Javascript";
+// const numbers = [1,2,3,4];
+// function tong(x,y){
+//     console.log(x+y);
+// }
+// export default class Person{
+//     constructor(id,name){
+//         this.id = id;
+//         this.name = name;
+//     }
+//     infor(){
+//         console.log(this.id+" _ "+this.name);
+//     }
+// }
+// export{ 
+//     numbers as mangSo,//đổi tên mang numbers thành mangso
+//     tong
+// }
+
+
+export let  User_name = "Javascript";
+const number = [1,2,3,4,5,6,7,8,9,10,11,12];
 function tong(x,y){
     console.log(x+y);
 }
@@ -9,11 +29,8 @@ export default class Person{
         this.id = id;
         this.name = name;
     }
-    infor(){
-        console.log(this.id+" _ "+this.name);
+    info(){
+        console.log(this.id+' _ '+this.name);
     }
 }
-export{
-    numbers as mangSo,//đổi tên mang numbers thành mangso
-    tong
-}
+export{number as MangSo, tong}
