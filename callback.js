32a344a66ca52d8c7d8973d5e176b2f0c1299fc8
Tống Đức Nam <637849@sv.vnua.edu.vn>
2021-11-17 17:00:50 +0700

    callback and object prototypes

---
+++ callback.js
@@ -0,0 +1,23 @@
+// function tong(x,y){
+//     return x+y;
+// }
+// function tich(x,y){
+//     return x*y;
+// }
+// function tinhtoan(call){
+//     var kq = call(10,11);
+//     console.log(kq);
+// }
+// tinhtoan(tich);
+
+// var person ={
+//     name: 'Tong Duc Nam',
+//     run: function(){
+//         // var that =this;
+//        setTimeout(()=>{
+//         console.log('Toi ten la: '+this.name);
+//        },500);
+//     },
+
+// }
+// person.run();
