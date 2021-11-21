95f9bc8d17c4c20cfd4d22f21d1628acfec0fe57
Tống Đức Nam <637849@sv.vnua.edu.vn>
2021-11-21 20:55:22 +0700

    21-11

--- callback.js
+++ callback.js
@@ -21,3 +21,44 @@
 
 // }
 // person.run();
+
+
+
+
+// function vogao(callback)
+// {
+//     callback();
+// }
+// function nauCom(){
+//     console.log('nau com');
+// }
+// vogao(nauCom);
+
+
+// function bieuthuc(num1,num2,cb)
+// {
+//     let kq = cb(num1,num2);
+//     console.log('Ket qua cua bieu thuc la: '+kq);
+// }
+// function tich(num1,num2)
+// {
+//     return num1*num2;
+// }
+// function hieu(num1,num2)
+// {
+//     return num1-num2;
+// }
+// function tong(num1,num2)
+// {
+//     return num1+num2;
+// }
+// bieuthuc(15,9,tong);
+
+// var array = ['Hello' , ' ', 'there', ' ', 'REPL'];
+// var message = ' ';
+// for (let word of array) {
+//     message += word;
+// }
+// message += '!';
+
+// console.log(message);
